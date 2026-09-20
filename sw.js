f8169cccd4531d2287ad906191ae11417604ae09
nirmalvasudevan <nirmalvasudevan@gmail.com>
2026-09-20 17:29:29 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Offline support: serve the last good copy when the network is weak.
-const CACHE = "ssb-desk-v6";
-const CORE = ["./", "./index.html", "./programs.json", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./logo.jpg"];
+const CACHE = "ssb-desk-v7";
+const CORE = ["./", "./index.html", "./programs.json", "./manifest.webmanifest", "./icon-v2-192.png", "./icon-v2-512.png", "./logo-v2.jpg"];
 self.addEventListener("install", (e) => { e.waitUntil(caches.open(CACHE).then((c) => c.addAll(CORE)).then(() => self.skipWaiting())); });
 self.addEventListener("activate", (e) => { e.waitUntil(caches.keys().then((ks) => Promise.all(ks.filter((k) => k !== CACHE).map((k) => caches.delete(k)))).then(() => self.clients.claim())); });
 self.addEventListener("fetch", (e) => {
